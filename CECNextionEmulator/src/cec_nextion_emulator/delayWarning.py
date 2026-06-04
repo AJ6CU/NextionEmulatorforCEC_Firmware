7faa5cae0f862ed0829a5eba6638d50bc2253e40
Mark Hatch <58341254+mark2382@users.noreply.github.com>
2026-06-03 17:23:03 -0700

    had to delete files to get new generated code from pygubu-designer

--- CECNextionEmulator/src/cec_nextion_emulator/delayWarning.py
+++ CECNextionEmulator/src/cec_nextion_emulator/delayWarning.py
@@ -8,35 +8,16 @@ UI source file: progress_warning.ui
 """
 import tkinter as tk
 import tkinter.ttk as ttk
+import delayWarningui as baseui
 
 
-class delayWarning(tk.Toplevel):
-    """Your widget direct subclass.
+#
+# Manual user code
+#
 
-    Only simple properties will be configured.
-    No commands, no bindings.
-    """
-
-    def __init__(self, *args, **kw):
-        super().__init__(*args, **kw)
-
-        self.warningFrame = ttk.Frame(self, name="warningframe")
-        self.warningFrame.configure(
-            height=200, style="Normal.TFrame", width=200)
-        self.warningLabel = ttk.Label(self.warningFrame, name="warninglabel")
-        self.warningLabel_VAR = tk.StringVar(
-            value='Loading...\n\nThis could take several seconds...')
-        self.warningLabel.configure(
-            anchor="w",
-            justify="center",
-            style="Heading2bi.TLabel",
-            text='Loading...\n\nThis could take several seconds...',
-            textvariable=self.warningLabel_VAR)
-        self.warningLabel.pack(padx=10, pady=10, side="top")
-        self.warningFrame.pack(expand=True, fill="both", side="top")
-        self.configure(height=200, width=200)
-        self.title("Warning... Delay")
-        # Layout for 'delay_warning' skipped in custom widget template.
+class delayWarning(baseui.delayWarningUI):
+    def __init__(self, master=None, **kw):
+        super().__init__(master, **kw)
 
 
 if __name__ == "__main__":
